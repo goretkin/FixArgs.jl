7f7b5bdd880dce5f1c3b2dbf5669a980368a1028
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-06-09 04:45:23 +0000

    build based on 49ced66

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Curry.jl-1","page":"Home","title":"Curry.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"Documentation for Curry.jl","category":"page"},{"location":"#","page":"Home","title":"Home","text":"CurrentModule = Curry\nDocTestSetup = quote\n    using Curry\nend","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [Curry]\nOrder   = [:function, :type]","category":"page"},{"location":"#Curry.interleave-Tuple{Any,Any}","page":"Home","title":"Curry.interleave","text":"Return a Tuple that interleaves args into the nothing slots of slots.\n\nCurry.interleave((:a, nothing, :c), (2,))\n\n# output\n\n(:a, 2, :c)\n\n\n\n\n\n","category":"method"}]
+}
