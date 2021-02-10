9e95d2f14511a5936b6b2be4e135f6ebb4e75a8e
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2021-02-10 18:09:25 -0500

    Inline an `include`

--- src/FixArgs.jl
+++ src/FixArgs.jl
@@ -1,6 +1,23 @@
 module FixArgs
 
-include("new.jl")
+module New
+
+# export so that Julia v1.6 type alias printing works
+# but not FixNew to not inhibit other type aliases: https://github.com/JuliaLang/julia/issues/39492
+export Fix1, Fix2
+
+using FrankenTuples: FrankenTuples, FrankenTuple
+
+include("parse.jl")
+include("expr.jl")
+include("show.jl")
+include("types.jl")
+include("eval.jl")
+include("TypedExpressions.jl")
+include("uneval.jl")
+include("ergonomics.jl")
+end
+
 using .New
 export Fix1, Fix2
 
