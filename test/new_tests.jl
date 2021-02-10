0bed8e9fa53c40ecfe3dec6675650a2ad6dc42fc
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2021-02-10 18:05:12 -0500

    Remove cruft

--- test/new_tests.jl
+++ test/new_tests.jl
@@ -119,57 +119,6 @@ end
     @test true
 end
 
-using FixArgs.New: fix, @fix
-@testset "basics" begin
-    @test  fix(≈, 1, nothing, atol=1.1)(2)
-    #@test  fix(≈, 1, nothing, )(2, atol=1.1)
-    #@test !fix(≈, 1, nothing, atol=1.1)(2, atol=0.9)
-    @test !fix(≈, 1, nothing, atol=0.9)(2)
-    #@test  fix(≈, 1, nothing, atol=0.9)(2, atol=1.1)
-    #@test  fix(≈, 1, nothing, atol=0.9)(2, atol=2)
-
-    @test fix(≈, nothing, nothing, atol=1.1)(1,2)
-    @test !fix(≈, nothing, nothing, atol=0.9)(1,2)
-
-    @test  (@fix ≈(1, _, atol=1.1))(2)
-    #@test  (@fix ≈(1, _)          )(2, atol=1.1)
-    #@test !(@fix ≈(1, _, atol=1.1))(2, atol=0.9)
-    @test !(@fix ≈(1, _, atol=0.9))(2)
-    #@test  (@fix ≈(1, _, atol=0.9))(2, atol=1.1)
-    #@test  (@fix ≈(1, _, atol=0.9))(2, atol=2)
-    @test  (@fix ≈(_, _, atol=1.1))(1,2)
-    @test !(@fix ≈(_, _, atol=0.9))(1,2)
-
-    x = "hello"
-    @test @fix(identity(_))(x) === x
-    xs = [1, 2]
-    @test @fix(+(_, xs...))(2) === 2 + 1 + 2
-    @test @fix(_ + 1 + _)(2,2) === 2 + 1 + 2
-
-    kw = (atol=1, rtol=0)
-    @test  (@fix ≈(_,_;kw...))(1,2)
-    @test !(@fix ≈(_,_;kw...))(1,2.1)
-
-    f(args...; kw...) = (args, kw.data)
-    @test (@fix f(1, _, 3, x=1, y=2))(2) === ((1,2,3),(x=1,y=2))
-    kw = (x=1, y=42)
-    #@test (@fix f(1, _, 3; kw...))(2, y=2) === ((1,2,3),(x=1,y=2))
-
-
-    a2 = @inferred fix(≈, nothing, nothing)
-    b2 = @inferred fix(≈, nothing, nothing, atol=1)
-    c2 = @inferred fix(≈, nothing, nothing, atol=1, rtol=2)
-    a1 = @inferred fix(≈, Some(3), nothing, atol=1, rtol=2)
-    @inferred a2(1,2)
-    @inferred b2(1,2)
-    @inferred c2(1,2)
-    @inferred a1(1.0)
-
-
-    @test_throws Exception @fix(_ + _)(1, 2, 3)
-    @test_throws Exception @fix(_ + _)(1)
-end
-
 @testset "type macro" begin
     @test (FixArgs.@FixT string(::Int64)) === typeof(@xquote string(3))
     v = @xquote string(1, 2::::S, (:three), (:four)::::S)
@@ -185,25 +134,4 @@ end
     @test FixArgs.New.xeval(frac2) === 1 / 2
     @test sizeof(frac) == 16
     @test sizeof(frac2) == 8
-end
-
-
-macro _test1(ex)
-    quote
-        ($ex, $(esc(ex)))
-    end
-end
-# using .New: EscVal, all_typed, uneval
-#=
-_ex_1 = :(x -> ==(x, 0))
-_ex_2 = :(x -> $(==)(x, 0))
-_ex_3 = :(x -> $(==)(x, :zero))
-_ex_4 = :(x -> $(==)(x, $(Val(0))))
-_ex_5 = :(x -> $(==)(x, $(EscVal{Val(0)}())))
-=#
-# ex = all_typed(_ex_2)
-
-if VERSION >= v"1.6-"
-    # test alias printing
-    # @test string(typeof(ex)) == "FixNew{Tuple{Val{:x}}, typeof(==), Tuple{Val{:x}, Int64}}"
 end
