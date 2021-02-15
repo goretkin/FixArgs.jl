3be1f35d014d72a81f1ef82712c97036618b7d04
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2021-02-15 16:26:38 -0500

    Move

--- src/ergonomics.jl
+++ src/ergonomics.jl
@@ -258,3 +258,31 @@ macro fix(call)
     end
     esc(ret)
 end
+
+function parse_type_spec(ex)
+    dc = _parse_double_colon(ex)
+    (!isnothing).(dc) == (false, true) && return (something(dc[2]), Some)
+    (!isnothing).(dc) == (true, false, true) && something(dc[3]) === :S && return (something(dc[1]), Val)
+    throw(Base.Meta.ParseError("expected a `::T`, got $ex"))
+end
+
+"""
+`@fix union([1], [2])` operates on values to produce an instance, whereas
+`@FixT union(::Vector{Int64}, ::Vector{Int64})` produces `typeof(@fix union([1], [2]))`
+"""
+macro FixT(ex)
+    try
+        Meta.isexpr(ex, :call) || throw(Base.Meta.ParseError("expected a call, got $ex"))
+        f = ex.args[1]
+        arg_types = map(parse_type_spec, ex.args[2:end])
+        arg_types_wrapped = map(((ex, wrap),) -> :($(wrap){$(esc(ex))}), arg_types)
+        f_ex = esc(:(typeof($f)))
+        args = Expr(:curly, :Tuple, arg_types_wrapped...)
+        call_args = :(FrankenTuple{$(args), (), Tuple{}})
+        #return :(($(Fix)){$(esc(f_ex)), $(esc(args)), $(esc(kw))})
+        return :(Call{Some{$(f_ex)}, $(call_args)})
+    catch err
+        err isa Base.Meta.ParseError || rethrow(err)
+        throw(Base.Meta.ParseError("expected e.g. `f(::S, ::T)`, got $ex. Detail: $(err.msg)"))
+    end
+end
