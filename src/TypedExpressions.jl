9b2b0facdf4834ef6d4c129a8b3fe2ca9b275036
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2022-06-20 16:03:33 -0400

    Remove `TypedExpr`
    TypedExpr probably burdened the compiler a lot, and allowed for abuse of method dispatch as pattern matching.
    Replacing it with a faux-switch statement is much more straightforward.

--- src/TypedExpressions.jl
+++ src/TypedExpressions.jl
@@ -1,61 +1,33 @@
-"""
-Roughly mirror `Base.Expr`, except that the the head of the expression (encoded in the `head` field)
-can be dispatched on.
-
-This is only used in an intermediate representation of this package.
-
-Note that `Expr` and `TypedExpr` are constructed slightly differently.
-Each argument of an `Expr` is an argument to `Expr`, whereas
-all arguments of a `TypedExpr` are passed as one argument (a tuple) to `TypedExpr`
-
-e.g.
-
-`Expr(:call, +, 1, 2)` corresponds to
-`TypedExpr(Val{:call}(), (+, 1, 2))`
-"""
-struct TypedExpr{H, A}
-    head::H
-    args::A
-end
-
-function TypedExpr(expr::Expr)
-    TypedExpr(
-        typed_expr(expr.head),
-        typed_expr(expr.args)
-    )
-end
-
-typed_expr(args::Vector) = tuple(map(typed_expr, args)...)
-typed_expr(sym::Symbol) = Val(sym)
-typed_expr(expr::Expr) = TypedExpr(expr)
-typed_expr(x) = x
-
-function inv_typed_expr(expr::TypedExpr)
-    Expr(
-        inv_typed_expr(expr.head),
-        inv_typed_expr(expr.args)...
-    )
-end
-
-inv_typed_expr(args::Tuple) = map(inv_typed_expr, [args...])
-inv_typed_expr(val::Val{sym}) where sym = sym
-inv_typed_expr(x) = x
-
-lc_expr(expr::TypedExpr{Val{:->}, Tuple{A, B}}) where {A, B} = Lambda(lc_expr(expr.args[1]), lc_expr(expr.args[2]))
-
-function lc_expr(expr::TypedExpr{Val{:call}, X}) where {X}
-    _expr = inv_typed_expr(expr)
-    (_args, _kwargs) = _extract_args_kwargs__collect_all_kw_into_kwargs(_expr.args[2:end])
-    f = lc_expr(expr.args[1])
-    args = map(lc_expr ∘ typed_expr, tuple(_args...))  # TODO ensure it's a `::Tuple`
-    kwargs =  _kwargs_to_named_tuple(_kwargs)
-    Call(f, FrankenTuple(args, kwargs))
-end
-
-lc_expr(expr::TypedExpr{Val{:tuple}, X}) where {X} = map(lc_expr, expr.args)
-lc_expr(expr::TypedExpr{Val{:escape}, X}) where {X} = inv_typed_expr(expr)
 
 """
-Convert a `::TypedExpr` to a Lambda-Call expression
+Convert an expression (e.g. an `Expr`) to a Lambda-Call value
 """
-lc_expr(x) = x
+lc_expr2(x) = x
+function lc_expr2(expr::Expr)
+    if expr.head === :->
+        length(expr.args) == 2 || error()
+        return Lambda(lc_expr2(expr.args[1]), lc_expr2(expr.args[2]))
+    end
+
+    if expr.head === :call
+        #_expr = inv_typed_expr(expr) # no more needed?
+        _expr = expr
+        (_args, _kwargs) = _extract_args_kwargs__collect_all_kw_into_kwargs(_expr.args[2:end])
+        f = lc_expr2(expr.args[1])
+        args = map(lc_expr2, tuple(_args...))  # TODO ensure it's a `::Tuple`
+        kwargs =  _kwargs_to_named_tuple(_kwargs)
+        return Call(f, FrankenTuple(args, kwargs))
+    end
+
+    if expr.head === :tuple
+        return map(lc_expr2, expr.args)
+    end
+
+    if expr.head === :escape
+        #_expr = inv_typed_expr(expr) # no more needed?
+        _expr = expr
+        return expr
+    end
+
+    return error()
+end
